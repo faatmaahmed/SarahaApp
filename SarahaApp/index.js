c0c2bd108b8e1d77da8dcccd86ed64d2ebc75033
faatmaahmed <faatmaa580@gmail.com>
2025-02-13 23:28:49 +0200

    index.js

--- SarahaApp/index.js
+++ SarahaApp/index.js
@@ -4,14 +4,13 @@ import  bootstrap  from './src/app.controller.js'
 import  express  from 'express'
 import { sendEmail } from './src/utils/email/sendEmails.js';
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 dotenv.config({path:path.join(path.resolve(),'./src/config/.env.prod')})
 const app = express()
 
 const PORT=process.env.PORT
 bootstrap(app , express)
 
-await sendEmail({to:"fatma.ahmed.mn3m@gmail.com"})
+await sendEmail({to:""})
 
 
 app.listen(PORT, () => 
